1f16b992a47a1a8f236d8d87a5614de2862894ec
unknown <lizale17@gmail.com>
2019-11-17 12:50:36 -0500

    arreglo

--- sigapdev-consultarecibos-front-master/src/Configuracion/Config.js
+++
@@ -1,5 +0,0 @@
-
-   const CONFIG= 'https://sigapdev-recibos-back.herokuapp.com/'
-   // const CONFIG= 'https://sigadev1-consultarecibos-back.herokuapp.com/' 
-   //const CONFIG= 'http://localhost:8080/';
-export default CONFIG;
