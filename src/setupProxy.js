fbd5889dd0b6e39df1ee82b81b7e8887a5b9d38f
unknown <lizale17@gmail.com>
2019-11-17 19:22:26 -0500

    Proxy v5

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        proxy('https://hongmengteam-tipodecambio-scra.herokuapp.com', 
+        proxy('/api', 
         { 
             target: 'https://hongmengteam-principal.herokuapp.com',
             secure: false,
